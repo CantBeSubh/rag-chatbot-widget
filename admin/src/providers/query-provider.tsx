10743ebb7ccd789d3442374a84c059627eebca1a
Subhranshu Pati <subhranshupati0412@gmail.com>
2026-06-26 13:19:50 +0530

    chore: devtool

--- admin/src/providers/query-provider.tsx
+++ admin/src/providers/query-provider.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -16,6 +17,10 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
   )
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+
   )
 }
